3069b214ad142510dafd93f95763aec0f4143007
vasudevapitta <tizzicboy@gmail.com>
2020-04-14 20:38:01 -0500

    copy files from build to public folder

---
+++ public/precache-manifest.967d410f76ab1c85552b87a760da0501.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1e98d60fe0f2130d1f4ef94a61c299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c15f0f16e28dacc5132",
+    "url": "/static/css/main.7d9dd359.chunk.css"
+  },
+  {
+    "revision": "4561fc0822175c485956",
+    "url": "/static/js/2.a38b823d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a38b823d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c15f0f16e28dacc5132",
+    "url": "/static/js/main.48cec4d4.chunk.js"
+  },
+  {
+    "revision": "ff3c0bb0ff7feacff290",
+    "url": "/static/js/runtime-main.442ad1b0.js"
+  },
+  {
+    "revision": "7212787ac2cb006948236e4600392180",
+    "url": "/static/media/EBGaramond-Regular.7212787a.ttf"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/static/media/fb.62d8b6ef.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/static/media/insta.e75317fa.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/static/media/linkedin.d9049f46.svg"
+  }
+]);
