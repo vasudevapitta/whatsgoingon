bda4200973e6602f63a134fbdeb45e9325eaf11a
vasudevapitta <tizzicboy@gmail.com>
2020-04-14 20:19:49 -0500

    Updates

---
+++ precache-manifest.a8df576328268428d2004ab3c7379a2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77878f4ded1cdcc760e5434362c121e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e96cafe3648866fdede",
+    "url": "/static/css/main.7d9dd359.chunk.css"
+  },
+  {
+    "revision": "4561fc0822175c485956",
+    "url": "/static/js/2.a38b823d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a38b823d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e96cafe3648866fdede",
+    "url": "/static/js/main.193f99f1.chunk.js"
+  },
+  {
+    "revision": "ff3c0bb0ff7feacff290",
+    "url": "/static/js/runtime-main.442ad1b0.js"
+  },
+  {
+    "revision": "7212787ac2cb006948236e4600392180",
+    "url": "/static/media/EBGaramond-Regular.7212787a.ttf"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/static/media/fb.62d8b6ef.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/static/media/insta.e75317fa.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/static/media/linkedin.d9049f46.svg"
+  }
+]);
